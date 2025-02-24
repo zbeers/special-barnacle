9db334f27f1393ee905ed63221141c0527fef0dc
Zachary Beers <zachary.beers1@gmail.com>
2025-02-24 05:58:29 -0500

    Update main.py

---
+++ app/main.py
@@ -0,0 +1,12 @@
+import sys
+
+def main()
+    #writes $ to prompt user input
+    sys.stdout.write('$ ')
+    
+    command = input()
+    print(f"{command}: command not found\n")
+    main() #makes main recursive
+
+if __name__ == '__main__':
+    main()
